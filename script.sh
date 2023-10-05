18fbfd4bab0740334c0585993b1b92cc58e6fdbb
mohamed_drayef <amindrayef@gmail.com>
2023-10-05 12:30:10 +0200

    Correction code + ajout de la demande du nom du fichier

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 read -p "Combien de fichiers voulez-vous crée : " fileQuantity
+read -p "Entrez le nom des fichiers : " fileName
 
 for i in $(seq 1 $fileQuantity);
 do
-    touch "fakeFile_$i"
+    touch "$fileName$i"
 done
 echo "$fileQuantity fichiers ont été crée"
